c6534e34bbbf61d54e57346d093a3014da4e161a
UR01084A <r01084a@upla.edu.pe>
2025-11-21 13:05:15 -0500

    Add files via upload

--- Modelo/supabase.js
+++ Modelo/supabase.js
@@ -1,8 +1,6 @@
-// /Modelo/supabase.js
-// Usamos ESM para que funcione directo en navegador sin bundler.
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 export const supabase = createClient(
-  'https://yzrvqpzefjxfitwmbceg.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6cnZxcHplZmp4Zml0d21iY2VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzMDg1MTEsImV4cCI6MjA3Nzg4NDUxMX0.ezg4W5DCcX9mgu1Emap5BgqbwcKJZX5k-N8z4LzJwgo'
+  "https://opzoxqftpfvpvfchmglf.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wem94cWZ0cGZ2cHZmY2htZ2xmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3NDA3OTUsImV4cCI6MjA3ODMxNjc5NX0._JEAzXglOBw0vBD3uyVjH_88Y2YEhK2TXw2e8_zpQNc"
 );
+
